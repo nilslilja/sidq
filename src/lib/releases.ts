ca8c4ad26b99722347716bf48f51c346d2d36777
Nils Lilja <nilsliljan@gmail.com>
2026-08-23 00:48:34 +0200

    release: 0.1.6

--- src/lib/releases.ts
+++ src/lib/releases.ts
@@ -15,7 +15,7 @@ import type { Platform } from './platform';
  * come out of the same bandwidth as the marketing page.
  */
 
-export const RELEASE_VERSION = '0.1.5';
+export const RELEASE_VERSION = '0.1.6';
 
 /*
  * Where the files are served from.
