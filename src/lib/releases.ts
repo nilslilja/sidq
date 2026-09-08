d7a1a58b4ac879f9bcec19b9a875a0ce2159e10f
Nils Lilja <nilsliljan@gmail.com>
2026-09-08 19:43:44 +0200

    chore: release 0.9.1

--- src/lib/releases.ts
+++ src/lib/releases.ts
@@ -15,7 +15,7 @@ import type { Platform } from './platform';
  * come out of the same bandwidth as the marketing page.
  */
 
-export const RELEASE_VERSION = '0.9.0';
+export const RELEASE_VERSION = '0.9.1';
 
 /*
  * Where the files are served from.
